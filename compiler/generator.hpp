0720760049aa752cc8034ad30e305585763d2c03
ImLecus <marcosgf2005@gmail.com>
2023-09-24 12:07:17 +0200

    Added linker

--- compiler/generator.hpp
+++ compiler/generator.hpp
@@ -3,18 +3,18 @@
 class Generator
 {
 public:
-    Generator(ExitStatement stat) : m_stat(move(stat)) {}
+    Generator(Exit stat) : m_stat(move(stat)) {}
     string generate()
     {
         /* Generates assembly code from the values of the AST */
         string assembly;
-        assembly += "global _start\n_start:\n";
-        assembly += "   mov rax, 60\n";
-        assembly += "   mov rdi, " + m_stat.value.literal.value + "\n";
+        assembly += "   global _main\n  section '.text'\n_main:\n";
+        assembly += "   MOV rax, 60\n";
+        assembly += "   MOV rdi, " + m_stat.value.literal.value + "\n";
         assembly += "   syscall";
         return assembly;
     }
 
 private:
-    ExitStatement m_stat;
+    Exit m_stat;
 };
